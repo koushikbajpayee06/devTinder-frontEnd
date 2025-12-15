795ac9b28b607d01e327983b76cd7f5f1c92bd1d
Koushik <Koushikbajpayee06@gmail.com>
2025-12-15 14:51:27 +0530

    Add NavBar component to App.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">DevTinder</a>
+        <a className="btn btn-ghost text-xl">🧑🏻‍💻 DevTinder</a>
       </div>
       <div className="flex gap-2">
         <input
@@ -12,7 +12,7 @@ const Navbar = () => {
           placeholder="Search"
           className="input input-bordered w-24 md:w-auto"
         />
-        <div className="dropdown dropdown-end">
+        <div className="dropdown dropdown-end mx-5">
           <div
             tabIndex={0}
             role="button"
