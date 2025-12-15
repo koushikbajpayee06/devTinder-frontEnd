0efabfb5131cbb32a452d959ebb3bdb52327823b
Koushik <Koushikbajpayee06@gmail.com>
2025-12-15 15:34:15 +0530

    feat: set up React Router with layout and outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Body from './components/Body'
+import Login from './components/login'
+import Profile from './components/Profile'
+
 
 const App = () => {
   return (
     <div > 
-      <Navbar/>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path='/' element={<Body />}>
+            <Route path='/login' element={ <Login/> }/>
+            <Route path='/profile' element={<Profile/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      
+
     </div>
   )
 }
