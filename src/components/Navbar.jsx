51c0bf3ce9b5568623593cb6cb0b2f499ee250c3
Koushik <Koushikbajpayee06@gmail.com>
2025-12-17 23:54:27 +0530

    after logout redirect to login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
 import axios from "axios";
 import React from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import { BASE_URL } from '../utils/constants'
+import { removeUser } from "../utils/userSlice";
 
 const Navbar = () => {
     const user = useSelector((store)=>store.user);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     console.log(user);
     const handleLogout=async()=>{
       try{
-        await axios.post(
-          BASE_URL+ "/logout",
-          {},
-          {withCredentials:true});
+        const res = await axios.post(BASE_URL+ "/logout",{},{withCredentials:true});
+        dispatch(removeUser());
+        return navigate("login");
 
       }catch(err){
         // Error logic maybe redirect to error page
@@ -62,4 +64,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Navbar
