f231c7380e950e1a89aff44086a95a568c984061
Koushik <Koushikbajpayee06@gmail.com>
2025-12-20 09:36:39 +0530

    signup api implemented  successfully

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,9 +7,11 @@ import { BASE_URL } from "../utils/constants";
 
 
 const Login = () => {
-
-    const [emailId, setEmailId] = useState("kbaju07@gmail.com");
-    const [password, setPasssword] = useState("Koushik@1234");
+    const[firstName,setFirstName] = useState("");
+    const[lastName,setLastName] = useState("");
+    const [emailId, setEmailId] = useState("");
+    const [password, setPasssword] = useState("");
+    const [isLoginForm, setIsLoginForm] = useState(false)
     const [error, setError] = useState("")
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -32,14 +34,54 @@ const Login = () => {
         }
 
     }
-    
+    const handleSignUp = async() =>{
+      try{
+        const res = await axios.post(BASE_URL + "/signup",
+          {firstName, lastName, emailId,password},
+          {withCredentials:true}
+        );
+        console.log(res.data.data)
+        dispatch(addUser(res.data.data));
+        return navigate('/profile');
+      }catch(err){
+        setError(err?.response?.data || "Something went wrong")
+      }
+    }
   return (
     <div className="flex justify-center my-10">
       <div className="card bg-base-300 w-96 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title justify-center">Login</h2>
+          <h2 className="card-title justify-center">{isLoginForm ? "Login": "SignUp"}</h2>
           <div>
-            <label className="form-control w-full max-w-xs my-2">
+           
+            {!isLoginForm && (
+              <>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text">First Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={firstName}
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e)=>setFirstName(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text">Last Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={lastName}
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e)=>setLastName(e.target.value)}
+                  />
+                </label>
+              </>)}
+             <label className="form-control w-full max-w-xs my-2">
               <div className="label">
                 <span className="label-text">Email ID</span>
               </div>
@@ -66,8 +108,12 @@ const Login = () => {
           </div>
           <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center m-2">
-            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+            <button className="btn btn-primary" onClick={isLoginForm ?handleLogin: handleSignUp}>{isLoginForm ?"Login":"Sign UP"}</button>
           </div>
+          <p className="m-auto pointer py-2" onClick={()=>setIsLoginForm((value)=>!value)}>
+            {isLoginForm 
+          ? "New User ? Signup here" 
+          : "Existing User?Login Here"}</p>
         </div>
       </div>
     </div>
